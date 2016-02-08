98f19cc6dc361ba0579cc58f6437653de90540ca
Dimitar Bounov <dimitar.bounov@gmail.com>
2016-02-08 13:02:54 -0800

    Add tautology checking in the frontend

--- src/static/inv.js
+++ src/static/inv.js
@@ -41,6 +41,18 @@ function equivalentPairs(invL1, invL2, cb) {
   return rpc.call("App.equivalentPairs", [ $.map(invL1, esprima.parse), $.map(invL2, esprima.parse) ], cb, log)
 }
 
+function impliedPairs(invL1, invL2, cb) {
+  return rpc.call("App.impliedPairs", [ $.map(invL1, esprima.parse), $.map(invL2, esprima.parse) ], cb, log)
+}
+
+function implied(invL1, inv, cb) {
+  return impliedPairs(invL1, [inv], function (res) { cb(res.length > 0) });
+}
+
+function isTautology(inv, cb) {
+  return rpc.call("App.isTautology", [ esprima.parse(inv) ], cb, log)
+}
+
 function invEval(inv, data) {
   // Sort variable names in order of decreasing length
   vars = data.variables.map(function(val, ind, _) { return [val, ind]; })
