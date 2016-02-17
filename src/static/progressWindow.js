f74b8f74459d17d314140e69fda7f89b2e5f1c88
Dimitar Bounov <dbounov@cs.ucsd.edu>
2016-02-17 14:52:15 -0800

    Separate progress window from the tutorial code

---
+++ src/static/progressWindow.js
@@ -0,0 +1,40 @@
+function ProgressWindow(div) {
+  var progW = this;
+  var ctr = 0;
+  var invMap = {}
+
+  $(div).html("Discovered invariants:<br>" +
+    "<ul id='good-invariants' style='font-family:monospace;'></ul>")
+
+  invUL = $('#good-invariants')
+
+  progW.addInvariant = function (inv) {
+      $(invUL).append("<li class='good-invariant' id='good_" +
+        ctr + "'>" + invToHTML(inv) + "</li>")
+      invMap[inv] = $('#good_' + ctr)
+      ctr++;
+  }
+
+  progW.removeInvariant = function (inv) {
+      $(invMap[inv]).remove();
+  }
+
+  progW.markInvariant = function (inv, state) {
+      if (state == "checking") {
+      } else if (state == "duplicate") {
+      } else if (state == "tautology") {
+      } else if (state == "implied") {
+      } else if (state == "ok") {
+      }
+  }
+
+  progW.clear = function () {
+    $(invUL).html('')
+    invMap = {}
+    ctr = 0
+  }
+
+  progW.contains = function (inv) {
+    return invMap.hasOwnProperty(inv);
+  }
+}
