1117c698db8faf77a9f65908a98fd467dc2682e1
Dimitar Bounov <dimitar.bounov@gmail.com>
2016-05-12 13:39:06 -0700

    Converted progressWindow to TS

---
+++ src/static/ts/progressWindow.ts
@@ -0,0 +1,75 @@
+type invariantT = string;  
+interface IProgressWindow {
+  addInvariant(invariant: invariantT): void;
+  removeInvariant(invariant: invariantT): void;
+  markInvariant(invariant: invariantT, state: string): void;
+  clearMarks(): void;
+  clear(): void;
+  contains(invariant: invariantT):  boolean;
+}
+
+class ProgressWindow implements IProgressWindow {
+  invMap : { [ inv: string] : HTMLElement } = { };
+  container: HTMLElement;
+  ctr : number = 0;
+
+  constructor(public parent:  HTMLDivElement) {
+    $(this.parent).html("<div class='progressWindow box good centered positioned'>" +
+                      "Accepted expressions<br>" +
+                      "<ul id='good-invariants'></ul>"+
+                   "</div>");
+    this.container = $(this.parent).children("div")[0]
+  };
+
+  addInvariant(invariant: invariantT) : void {
+    let invUl = $(this.container).children("#good-invariants")[0]
+    $(invUl).append("<li class='good-invariant' id='good_" +
+      this.ctr + "'>" + invToHTML(invariant) + "</li>")
+    this.invMap[invariant] = $('#good_' + this.ctr)[0]
+    this.ctr ++;
+  }
+
+  removeInvariant(invariant: invariantT): void {
+    $(this.invMap[invariant]).remove();
+    delete this.invMap[invariant]
+  }
+
+  
+  markInvariant(invariant: invariantT, state: string): void {
+    let invDiv = $(this.invMap[invariant]);
+
+    if (invDiv == undefined) {
+      console.log("Unknown invariant " + invariant);
+      return
+    }
+
+      if (state == "checking") {
+      } else if (state == "duplicate") {
+        invDiv.addClass('error')
+      } else if (state == "tautology") {
+      } else if (state == "implies") {
+        invDiv.addClass('error')
+      } else if (state == "counterexampled") {
+        invDiv.addClass('error')
+      } else if (state == "ok") {
+        invDiv.removeClass('error')
+      }
+  }
+
+  clearMarks(): void {
+    for (let i in this.invMap) {
+      $(this.invMap[i]).removeClass("error")
+    }
+  }
+
+  clear(): void {
+    let invUL: HTMLElement = $(this.container).children("#good-invariants")[0]
+    $(invUL).html('')
+    this.invMap = {}
+    this.ctr = 0
+  }
+
+  contains(invariant: invariantT):  boolean {
+    return this.invMap.hasOwnProperty(invariant);
+  }
+}
