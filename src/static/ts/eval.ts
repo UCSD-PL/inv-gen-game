67e6163bdc959a3eac6850e047874d31894860ba
Rohit Jha <r1jha@ucsd.edu>
2016-05-16 21:23:11 -0700

    TypeScript linter

--- src/static/ts/eval.ts
+++ src/static/ts/eval.ts
@@ -1,240 +1,240 @@
 type strset = { [ ind: string ]: boolean }
-var notDefRe = /(.*) is not defined/;
+let notDefRe = /(.*) is not defined/;
 
-class InvException{
+class InvException {
   constructor(public name: string, public msg: string) { }
 }
 
 function interpretError(err: Error): (string|Error) {
-  if (err.name == 'ReferenceError') {
+  if (err.name === "ReferenceError") {
     if (err.message.match(notDefRe))
-      return err.message.match(notDefRe)[1] + " is not defined."
+      return err.message.match(notDefRe)[1] + " is not defined.";
     else
-      return "Not a valid expression."
-  } else if (err.name == 'SyntaxError') {
-    return "Not a valid expression."
-  } else if (err.name == 'NOT_BOOL') {
-    return "Expression should evaluate to true or false, not " + err.message + " for example."
+      return "Not a valid expression.";
+  } else if (err.name === "SyntaxError") {
+    return "Not a valid expression.";
+  } else if (err.name === "NOT_BOOL") {
+    return "Expression should evaluate to true or false, not " + err.message + " for example.";
   }
 
-  return err
+  return err;
 }
 
-function invToJS(inv:string): string {
-  return inv.replace(/[^<>]=[^>]/g, function (v) { return v[0] + '==' + v[2]; })
+function invToJS(inv: string): string {
+  return inv.replace(/[^<>]=[^>]/g, function (v) { return v[0] + "==" + v[2]; });
 }
 
-function invEval(inv:string, variables: string[], data: any[][]): any[] {
+function invEval(inv: string, variables: string[], data: any[][]): any[] {
   // Sort variable names in order of decreasing length
-  let vars:[string, number][] = variables.map(function(val, ind, _) { return <[string, number]>[val, ind]; })
-  vars.sort(function (e1,e2) {
+  let vars: [string, number][] = variables.map(function(val, ind, _) { return <[string, number]>[val, ind]; });
+  vars.sort(function (e1, e2) {
     let s1 = e1[0], s2 = e2[0];
     if (s1.length > s2.length) return -1;
     if (s1.length < s2.length) return 1;
     return 0;
-  })
+  });
 
-  let holds_arr = []
+  let holds_arr = [];
 
   // Do substitutions and eval
-  for (var row in data) {
-    let s = inv
-    for (var v in vars) {
-      s = s.replace(new RegExp(vars[v][0], 'g'), data[row][vars[v][1]])
+  for (let row in data) {
+    let s = inv;
+    for (let v in vars) {
+      s = s.replace(new RegExp(vars[v][0], "g"), data[row][vars[v][1]]);
     }
-    let res = eval(s)
-    holds_arr.push(res)
+    let res = eval(s);
+    holds_arr.push(res);
   }
-  return holds_arr
+  return holds_arr;
 }
 
 function evalResultBool(evalResult: any[]): boolean {
-  for (var i in evalResult) {
+  for (let i in evalResult) {
     if (evalResult[i] instanceof Array)
-      return evalResultBool(evalResult[i])
-    if (typeof(evalResult[i]) != "boolean")
+      return evalResultBool(evalResult[i]);
+    if (typeof(evalResult[i]) !== "boolean")
       return false;
   }
   return true;
 }
 
-function estree_reduce<T>(t: ESTree.Node, cb: (n:ESTree.Node, args:T[]) => T): T {
-  if (t.type == "Program") {
-    let p = <ESTree.Program>t
-    let es = <ESTree.ExpressionStatement>p.body[0]
-    return cb(t, [ estree_reduce<T>(es.expression, cb) ])
+function estree_reduce<T>(t: ESTree.Node, cb: (n: ESTree.Node, args: T[]) => T): T {
+  if (t.type === "Program") {
+    let p = <ESTree.Program>t;
+    let es = <ESTree.ExpressionStatement>p.body[0];
+    return cb(t, [ estree_reduce<T>(es.expression, cb) ]);
   }
 
-  if (t.type == "BinaryExpression") {
-    let be = <ESTree.BinaryExpression>t
-    let lhs: T = estree_reduce(be.left, cb)
-    let rhs: T = estree_reduce(be.right, cb)
-    return cb(t, [lhs, rhs])
+  if (t.type === "BinaryExpression") {
+    let be = <ESTree.BinaryExpression>t;
+    let lhs: T = estree_reduce(be.left, cb);
+    let rhs: T = estree_reduce(be.right, cb);
+    return cb(t, [lhs, rhs]);
   }
 
-  if (t.type == "LogicalExpression") {
-    let be = <ESTree.LogicalExpression>t
-    let lhs: T = estree_reduce(be.left, cb)
-    let rhs: T = estree_reduce(be.right, cb)
-    return cb(t, [lhs, rhs])
+  if (t.type === "LogicalExpression") {
+    let be = <ESTree.LogicalExpression>t;
+    let lhs: T = estree_reduce(be.left, cb);
+    let rhs: T = estree_reduce(be.right, cb);
+    return cb(t, [lhs, rhs]);
   }
 
-  if (t.type == "UnaryExpression") {
-    let ue = <ESTree.UnaryExpression>t
-    let exp: T = estree_reduce(ue.argument, cb)
-    return cb(t, [ exp ])
+  if (t.type === "UnaryExpression") {
+    let ue = <ESTree.UnaryExpression>t;
+    let exp: T = estree_reduce(ue.argument, cb);
+    return cb(t, [ exp ]);
   }
 
-  if (t.type == "Literal") {
+  if (t.type === "Literal") {
     return cb(t, []);
   }
 
-  if (t.type == "Identifier") {
+  if (t.type === "Identifier") {
     return cb(t, []);
   }
 
-  assert(false, "Shouldn't get here")
+  assert(false, "Shouldn't get here");
 }
 
-function identifiers(inv:(string|ESTree.Node)): strset {
-  if (typeof(inv) == "string")
-    return identifiers(esprima.parse(<string>inv))
+function identifiers(inv: (string|ESTree.Node)): strset {
+  if (typeof(inv) === "string")
+    return identifiers(esprima.parse(<string>inv));
 
-  let t = <ESTree.Node> inv
+  let t = <ESTree.Node> inv;
 
   return estree_reduce(t, (nd: ESTree.Node, args: strset[]) => {
-    if (nd.type == "Program")
-      return args[0]
+    if (nd.type === "Program")
+      return args[0];
 
-    if (nd.type == "BinaryExpression" || nd.type == "LogicalExpression") {
-      return union(args[0], args[1])
+    if (nd.type === "BinaryExpression" || nd.type === "LogicalExpression") {
+      return union(args[0], args[1]);
     }
 
-    if (nd.type == "UnaryExpression") {
-      return args[0]
+    if (nd.type === "UnaryExpression") {
+      return args[0];
     }
 
-    if (nd.type == "Identifier") {
-      let r: strset = {}
+    if (nd.type === "Identifier") {
+      let r: strset = {};
       r[(<ESTree.Identifier>nd).name] = true;
-      return r
+      return r;
     }
 
-    return {}
-  })
+    return {};
+  });
 }
 
-function literals(inv:(string|ESTree.Node)):strset {
-  if (typeof(inv) == "string")
-    return literals(esprima.parse(<string>inv))
+function literals(inv: (string|ESTree.Node)): strset {
+  if (typeof(inv) === "string")
+    return literals(esprima.parse(<string>inv));
 
   return estree_reduce(<ESTree.Node>inv, (nd: ESTree.Node, args: strset[]) => {
-    if (nd.type == "Program")
-      return args[0]
+    if (nd.type === "Program")
+      return args[0];
 
-    if (nd.type == "BinaryExpression" || nd.type == "LogicalExpression") {
-      return union(args[0], args[1])
+    if (nd.type === "BinaryExpression" || nd.type === "LogicalExpression") {
+      return union(args[0], args[1]);
     }
 
-    if (nd.type == "UnaryExpression") {
-      return args[0]
+    if (nd.type === "UnaryExpression") {
+      return args[0];
     }
 
-    if (nd.type == "Literal") {
-      let r: strset = {}
+    if (nd.type === "Literal") {
+      let r: strset = {};
       r[(<string>(<ESTree.Literal>nd).value)] = true;
-      return r
+      return r;
     }
 
-    return {}
-  })
+    return {};
+  });
 }
 
-function operators(inv:(string|ESTree.Node)):strset {
-  if (typeof(inv) == "string")
-    return operators(esprima.parse(<string>inv))
+function operators(inv: (string|ESTree.Node)): strset {
+  if (typeof(inv) === "string")
+    return operators(esprima.parse(<string>inv));
 
   return estree_reduce(<ESTree.Node>inv, (nd: ESTree.Node, args: strset[]) => {
-    if (nd.type == "Program")
-      return args[0]
+    if (nd.type === "Program")
+      return args[0];
 
-    if (nd.type == "BinaryExpression" || nd.type == "LogicalExpression") {
-      let be = <ESTree.BinaryExpression>nd
-      let p: strset = {}
+    if (nd.type === "BinaryExpression" || nd.type === "LogicalExpression") {
+      let be = <ESTree.BinaryExpression>nd;
+      let p: strset = {};
       p[be.operator] = true;
-      return union(union(p, args[0]), args[1])
+      return union(union(p, args[0]), args[1]);
     }
 
-    if (nd.type == "UnaryExpression") {
-      let ue =  <ESTree.UnaryExpression>nd
-      let p: strset = {}
+    if (nd.type === "UnaryExpression") {
+      let ue =  <ESTree.UnaryExpression>nd;
+      let p: strset = {};
       p[ue.operator] = true;
-      return union(p, args[0])
+      return union(p, args[0]);
     }
 
-    return {}
-  })
+    return {};
+  });
 }
 
-function replace(inv:(string|ESTree.Node), replF): ESTree.Node {
-  if (typeof(inv) == "string")
-    return replace(esprima.parse(<string>inv), replF)
+function replace(inv: (string|ESTree.Node), replF): ESTree.Node {
+  if (typeof(inv) === "string")
+    return replace(esprima.parse(<string>inv), replF);
 
   return estree_reduce(<ESTree.Node>inv, (nd: ESTree.Node, args: ESTree.Node[]): ESTree.Node => {
-    if (nd.type == "Program") {
-      let p = <ESTree.Program>nd
-      let es = <ESTree.ExpressionStatement>p.body[0]
+    if (nd.type === "Program") {
+      let p = <ESTree.Program>nd;
+      let es = <ESTree.ExpressionStatement>p.body[0];
       return <ESTree.Program>{ "type" : "Program",
-               "body": [ <ESTree.ExpressionStatement>{ 
+               "body": [ <ESTree.ExpressionStatement>{
                   "type": "ExpressionStatement",
                   "expression": replace(es.expression, replF)
                 } ],
                "sourceType": "dummy"
-             }
+             };
     }
 
-    if (nd.type == "BinaryExpression") {
+    if (nd.type === "BinaryExpression") {
       let be = <ESTree.BinaryExpression>nd;
       return replF(<ESTree.LogicalExpression>{ "type": "LogicalExpression",
                      "operator": be.operator,
                      "left": args[0],
                      "right": args[1],
-                  })
+                  });
     }
 
-    if (nd.type == "LogicalExpression") {
+    if (nd.type === "LogicalExpression") {
       let le = <ESTree.LogicalExpression>nd;
       return replF(<ESTree.LogicalExpression>{ "type": "LogicalExpression",
                      "operator": le.operator,
                      "left": args[0],
                      "right": args[1],
-                  })
+                  });
     }
 
-    if (nd.type == "UnaryExpression") {
+    if (nd.type === "UnaryExpression") {
       let ue = <ESTree.UnaryExpression>nd;
       return replF(<ESTree.UnaryExpression>{ "type": "UnaryExpression",
                      "operator": ue.operator,
                      "argument": args[0],
-             })
+             });
     }
 
-    if (nd.type == "Literal" || nd.type == "Identifier") {
-      return replF(nd)
+    if (nd.type === "Literal" || nd.type === "Identifier") {
+      return replF(nd);
     }
-  })
+  });
 }
 
-function abstractLiterals(inv:string|ESTree.Node): [ESTree.Node, string[]] {
-  var newVars: string[] = [];
-  var newInv: ESTree.Node = replace(inv, (node) => {
-    if (node.type == "Literal") {
-      var newId = "a" + newVars.length
-      newVars.push(newId)
-      return { "type": "Identifier", "name": newId }
+function abstractLiterals(inv: string|ESTree.Node): [ESTree.Node, string[]] {
+  let newVars: string[] = [];
+  let newInv: ESTree.Node = replace(inv, (node) => {
+    if (node.type === "Literal") {
+      let newId = "a" + newVars.length;
+      newVars.push(newId);
+      return { "type": "Identifier", "name": newId };
     }
 
-    return node
-  })
-  return [ newInv, newVars ]
+    return node;
+  });
+  return [ newInv, newVars ];
 }
--- src/static/ts/util.ts
+++ src/static/ts/util.ts
@@ -1,23 +1,23 @@
 type directionT = "up" | "down" | "left" | "right"
 function log(arg: any): void { console.log(arg); }
 function error(arg: any): void { log (arg); }
-function assert(c:boolean, msg?:any): void {
-  if (msg == undefined)
-    msg = "Oh-oh"
+function assert(c: boolean, msg?: any): void {
+  if (msg === undefined)
+    msg = "Oh-oh";
 
   if (!c)
-    throw msg || "Assertion failed."
+    throw msg || "Assertion failed.";
 }
 
 
-function fst<T1,T2>(x:[T1,T2]):T1 { return x[0]; }
-function snd<T1,T2>(x:[T1,T2]):T2 { return x[1]; }
+function fst<T1, T2>(x: [T1, T2]): T1 { return x[0]; }
+function snd<T1, T2>(x: [T1, T2]): T2 { return x[1]; }
 
-function shuffle<T>(arr:T[]): void {
-    var j;
-    for (var i = 0; i < arr.length; i++) {
+function shuffle<T>(arr: T[]): void {
+    let j: number = null;
+    for (let i: number = 0; i < arr.length; i++) {
         j = Math.floor(Math.random() * arr.length);
-        var x = arr[i];
+        let x = arr[i];
         arr[i] = arr[j];
         arr[j] = x;
     }
@@ -39,69 +39,69 @@ class Label {
     } else {
       this.pos = {
         of: <HTMLElement>pos_arg
-      }
-      if (direction == "up") {
-        this.pos['at'] = 'center bottom'
-      } else if (direction == "down") {
-        this.pos['at'] = 'center top'
-      } else if (direction == "left") {
-        this.pos['at'] = 'right center'
-      } else if (direction == "right") {
-        this.pos['at'] = 'left center'
+      };
+      if (direction === "up") {
+        this.pos["at"] = "center bottom";
+      } else if (direction === "down") {
+        this.pos["at"] = "center top";
+      } else if (direction === "left") {
+        this.pos["at"] = "right center";
+      } else if (direction === "right") {
+        this.pos["at"] = "left center";
       }
     }
 
     let clazz: string = "", text_pos: string = "", arrow_pos: string = "",
         arrow_div_pos: string = "", arrow_div_pos1: string = "";
 
-    if (direction == "up") {
-      clazz = "arrow_up"
-      text_pos = "top:  30px;"
-      arrow_pos = "left:  20px; top:  20px;"
-      arrow_div_pos = "left-10 top"
-      arrow_div_pos1 = "left-10 top+" + pulseWidth
-    } else if (direction == "down") {
-      clazz = "arrow_down"
-      text_pos = "top:  0px;"
-      arrow_pos = "left:  20px; top:  40px;"
-      arrow_div_pos = "center bottom"
-      arrow_div_pos1 = "center bottom-" + pulseWidth
-    } else if (direction == "left") {
-      clazz = "arrow_left"
-      text_pos = "left:  30px; top: -10px;"
-      arrow_pos = "left:  0px; top:  0px;"
-      arrow_div_pos = "left top-7"
-      arrow_div_pos1 = "left+" + pulseWidth + " top-7"
-    } else if (direction == "right") {
-      clazz = "arrow_right"
-      text_pos = "float:  left;"
-      arrow_pos = "float: right;"
-      arrow_div_pos = "right center"
-      arrow_div_pos1 = "right-" + pulseWidth +" center"
+    if (direction === "up") {
+      clazz = "arrow_up";
+      text_pos = "top:  30px;";
+      arrow_pos = "left:  20px; top:  20px;";
+      arrow_div_pos = "left-10 top";
+      arrow_div_pos1 = "left-10 top+" + pulseWidth;
+    } else if (direction === "down") {
+      clazz = "arrow_down";
+      text_pos = "top:  0px;";
+      arrow_pos = "left:  20px; top:  40px;";
+      arrow_div_pos = "center bottom";
+      arrow_div_pos1 = "center bottom-" + pulseWidth;
+    } else if (direction === "left") {
+      clazz = "arrow_left";
+      text_pos = "left:  30px; top: -10px;";
+      arrow_pos = "left:  0px; top:  0px;";
+      arrow_div_pos = "left top-7";
+      arrow_div_pos1 = "left+" + pulseWidth + " top-7";
+    } else if (direction === "right") {
+      clazz = "arrow_right";
+      text_pos = "float:  left;";
+      arrow_pos = "float: right;";
+      arrow_div_pos = "right center";
+      arrow_div_pos1 = "right-" + pulseWidth + " center";
     }
 
     let div = $("<div class='absolute'><div class=" + clazz +
       " style='" + arrow_pos + "'></div><div style='" + text_pos +
-      "position: relative; text-align: center;'>" + txt + "</div></div>")
+      "position: relative; text-align: center;'>" + txt + "</div></div>");
 
-    $('body').append(div);
+    $("body").append(div);
 
-    let arrowDiv = $(div).children('div')[0]
+    let arrowDiv = $(div).children("div")[0];
     let apos = $(arrowDiv).position();
 
-    this.pos.my = arrow_div_pos
-    $(div).position(this.pos)
+    this.pos.my = arrow_div_pos;
+    $(div).position(this.pos);
     this.pos.using = (css, dummy) => $(div).animate(css, pulse / 2);
 
     let ctr = 0;
     let lbl = this;
 
     this.timer = setInterval(function () {
-      let v = (ctr % 2 == 0 ? arrow_div_pos : arrow_div_pos1)
+      let v = (ctr % 2 === 0 ? arrow_div_pos : arrow_div_pos1);
       lbl.pos.my = v;
-      $(div).position(lbl.pos)
+      $(div).position(lbl.pos);
       ctr ++;
-    }, this.pulse / 2)
+    }, this.pulse / 2);
     this.elem = div;
   }
 
@@ -110,41 +110,41 @@ class Label {
     clearInterval(this.timer);
   }
 }
-function removeLabel(l:Label) {
+function removeLabel(l: Label) {
   l.remove();
 }
 
 function label(arg: (JQueryUI.JQueryPositionOptions | HTMLElement),
                txt: string,
                direction: directionT) {
-  return new Label(arg, txt, direction)
+  return new Label(arg, txt, direction);
 }
 
 interface IStep {
-  setup(cs:any):any;
+  setup(cs: any): any;
 }
 
 class Script {
   step: number = -1;
-  cancelCb: ()=>void = null;
-  timeoutId: any
+  cancelCb: () => void = null;
+  timeoutId: any;
 
   constructor(public steps: IStep[]) { this.nextStep(); }
   nextStep(): void {
     this.step++;
     this.cancelCb = null;
-    this.steps[this.step].setup(this)
+    this.steps[this.step].setup(this);
   }
 
   nextStepOnKeyOrTimeout(timeout: number,
-                         destructor: ()=>any,
+                         destructor: () => any,
                          keyCode: number = null) {
     let s = this;
     if (timeout > 0) {
      this.timeoutId = setTimeout(function() {
        destructor();
-       $('body').off('keyup');
-       $('body').off('keypress');
+       $("body").off("keyup");
+       $("body").off("keypress");
        s.nextStep();
      }, timeout);
     }
@@ -152,22 +152,22 @@ class Script {
     this.cancelCb = function () {
      if (timeout > 0)
        clearTimeout(this.timeoutId);
-     $('body').off('keyup');
-     $('body').off('keypress');
+     $("body").off("keyup");
+     $("body").off("keypress");
      destructor();
-    }
+   };
 
-    $('body').keypress(function (ev) {
-     if (keyCode === null || ev.which == keyCode) {
+    $("body").keypress(function (ev) {
+     if (keyCode === null || ev.which === keyCode) {
        ev.stopPropagation();
        return false;
      }
-    })
+   });
 
-    $('body').keyup(function (ev) {
-     if (keyCode === null || ev.which == keyCode) {
-       $('body').off('keyup');
-       $('body').off('keypress');
+    $("body").keyup(function (ev) {
+     if (keyCode === null || ev.which === keyCode) {
+       $("body").off("keyup");
+       $("body").off("keypress");
        destructor();
        if (timeout > 0)
          clearTimeout(this.timeoutId);
@@ -186,74 +186,74 @@ class Script {
 }
 
 /* In-place union - modifies s1 */
-function union(s1:any, s2:any): any {
-  for (var e in s2) {
+function union(s1: any, s2: any): any {
+  for (let e in s2) {
     s1[e] = true;
   }
 
   return s1;
 }
 
-function setlen(s:any): number {
-  var l = 0;
-  for (var i in s) {
+function setlen(s: any): number {
+  let l = 0;
+  for (let i in s) {
     if (s.hasOwnProperty(i))  l++;
   }
   return l;
 }
 
 function forAll(boolL: boolean[]): boolean {
-  return boolL.map(x => x ? 1 : 0).reduce((x,y)=>x+y, 0) == boolL.length
+  return boolL.map(x => x ? 1 : 0).reduce((x, y) => x + y, 0) === boolL.length;
 }
 
-function zip<T1,T2>(a1:T1[], a2:T2[]): [T1,T2][] {
-  return a1.map((_,i:number) => <[T1,T2]>[a1[i], a2[i]])
+function zip<T1, T2>(a1: T1[], a2: T2[]): [T1, T2][] {
+  return a1.map((_, i: number) => <[T1, T2]>[a1[i], a2[i]]);
 }
 
 class KillSwitch {
   pos: number = 0;
-  onFlipCb: (i:number)=>any = (i)=>0;
-  container: JQuery
+  onFlipCb: (i: number) => any = (i) => 0;
+  container: JQuery;
 
   constructor(public parent:  HTMLElement) {
-    this.container = $('<div class="kill-switch" style="position: absolute;"></div>')
+    this.container = $("<div class='kill-switch' style='position: absolute;'></div>");
     let pOff = $(this.parent).offset();
     let pW =  $(this.parent).width();
     das.position(this.container[0], {
-      my: 'right center',
+      my: "right center",
       of: this.parent,
-      at: 'right-40 bottom'
-    })
-    $('body').append(this.container)
+      at: "right-40 bottom"
+    });
+    $("body").append(this.container);
 
     let ks = this;
     this.container.click(function () {
-      if (ks.pos == 0) {
+      if (ks.pos === 0) {
         ks.pos = 1;
       } else {
         ks.pos = 0;
       }
-      ks.refresh()
-      ks.onFlipCb(ks.pos)
-    })
-    this.refresh()
+      ks.refresh();
+      ks.onFlipCb(ks.pos);
+    });
+    this.refresh();
   }
 
-  onFlip(cb:(i:number)=>any): void {
-    this.onFlipCb = cb
+  onFlip(cb: (i: number) => any): void {
+    this.onFlipCb = cb;
   }
 
-  refresh(): void{
-    if (this.pos == 0) {
-      this.container.html("<img src='knife-up.gif' style='width: 30; height: 60px;'/>")
+  refresh(): void {
+    if (this.pos === 0) {
+      this.container.html("<img src='knife-up.gif' style='width: 30; height: 60px;'/>");
     } else {
-      this.container.html("<img src='knife-down.gif' style='width: 30; height: 60px;'/>")
+      this.container.html("<img src='knife-down.gif' style='width: 30; height: 60px;'/>");
     }
   }
 
   destroy(): void {
-    this.container.remove()
-    das.remove(this.container[0])
+    this.container.remove();
+    das.remove(this.container[0]);
   }
 
   flip(): void {
@@ -265,48 +265,48 @@ class DynamicAttachments {
   objs: [HTMLElement, JQueryUI.JQueryPositionOptions][] = [];
   position(target: HTMLElement, spec: JQueryUI.JQueryPositionOptions) {
     for (let i in this.objs) {
-      if (this.objs[i][0] == target) {
+      if (this.objs[i][0] === target) {
         this.objs[i][1] = spec;
-        $(target).position(spec)
-        return
+        $(target).position(spec);
+        return;
       }
     }
-    this.objs.push([target, spec])
-    $(target).position(spec)
+    this.objs.push([target, spec]);
+    $(target).position(spec);
   }
 
   reflowAll(): void {
-    for (var i in this.objs) {
-      $(this.objs[i][0]).position(this.objs[i][1])
+    for (let i in this.objs) {
+      $(this.objs[i][0]).position(this.objs[i][1]);
     }
   }
 
   remove(elmt: HTMLElement): void {
-    this.objs = this.objs.filter( (item) => item[0] != elmt )
+    this.objs = this.objs.filter( (item) => item[0] !== elmt );
   }
 }
 
-var das = new DynamicAttachments();
+let das = new DynamicAttachments();
 
 function disableBackspaceNav() {
-  $(document).unbind('keydown').bind('keydown', function (event) {
+  $(document).unbind("keydown").bind("keydown", function (event) {
     let doPrevent = false;
     if (event.keyCode === 8) {
       let d = event.srcElement || event.target;
-      if (d.tagName.toUpperCase() === 'INPUT') {
+      if (d.tagName.toUpperCase() === "INPUT") {
         let di = <HTMLInputElement>d;
-        if (di.type.toUpperCase() === 'TEXT' ||
-            di.type.toUpperCase() === 'PASSWORD' ||
-            di.type.toUpperCase() === 'FILE' ||
-            di.type.toUpperCase() === 'SEARCH' ||
-            di.type.toUpperCase() === 'EMAIL' ||
-            di.type.toUpperCase() === 'NUMBER' ||
-            di.type.toUpperCase() === 'DATE' ) {
+        if (di.type.toUpperCase() === "TEXT" ||
+            di.type.toUpperCase() === "PASSWORD" ||
+            di.type.toUpperCase() === "FILE" ||
+            di.type.toUpperCase() === "SEARCH" ||
+            di.type.toUpperCase() === "EMAIL" ||
+            di.type.toUpperCase() === "NUMBER" ||
+            di.type.toUpperCase() === "DATE" ) {
           doPrevent = di.readOnly || di.disabled;
         } else {
           doPrevent = true;
         }
-      } else if (d.tagName.toUpperCase() === 'TEXTAREA') {
+      } else if (d.tagName.toUpperCase() === "TEXTAREA") {
         let dta = <HTMLTextAreaElement>d;
         doPrevent = dta.readOnly || dta.disabled;
       } else {
