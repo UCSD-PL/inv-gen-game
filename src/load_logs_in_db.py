750dc8a69a8d5a4b262b07433b0f9466bb726ced
Dimo <lami4ka@gmail.com>
2016-10-18 19:40:36 -0700

    Cleanup models.py

---
+++ src/load_logs_in_db.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, DateTime, Sequence
+from sqlalchemy.orm import relationship, sessionmaker
+from json import loads, dumps
+from js import esprimaToBoogie
+from datetime import datetime
+
+import json
+import sys;
+
+if len(sys.argv) != 3:
+    print "Usage: <db_file> <log_file>"
+
+engine = create_engine('sqlite:///' + sys.argv[1], echo=True)
+Session = sessionmaker(bind=engine)
+
+s = Session()
+
+Base.metadata.create_all(engine);
+
+srcs = {
+    s.name : s for s in s.query(Source).all()
+}
+
+with open(sys.argv[2], 'r') as f:
+    for l in f:
+      e = json.loads(l)
+
+      if e["method"] != "logEvent":
+        continue;
+
+      time = e["time"]
+      ip = e['ip']
+      (worker_id, evt_type, rest) = e["args"]
+
+      if worker_id not in srcs:
+        src = Source(name=worker_id)
+        s.add(src);
+        srcs[worker_id] = src;
+
+      src = srcs[worker_id]
+      if (evt_type == "TutorialStart"):
+        payl = { }
+      elif (evt_type == "TutorialDone"):
+        payl = { }
+      elif (evt_type == "StartLevel" or evt_type == "FinishLevel"):
+        payl = { "lvlset" : rest[0], "lvlid" : rest[1] }
+        if evt_type == "FinishLevel":
+          payl["verified"] = rest[2]
+          invs = zip(rest[3], [ str(esprimaToBoogie(x, {})) for x in rest[4] ])
+          payl["all_found"] = invs;
+      elif (evt_type == "FoundInvariant"):
+        payl = [ rest[2], str(esprimaToBoogie(rest[3], { })) ]
+      else:
+        print "Unknown event: ", e
+      t = datetime.fromtimestamp(time)
+      evt = Event(type=evt_type, src=src.name, addr=ip, time=t, payload=dumps(payl))
+      s.add(evt);
+
+s.commit();
--- src/models.py
+++ src/models.py
@@ -1,10 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, DateTime, Sequence
 from sqlalchemy.orm import relationship, sessionmaker
-from json import loads, dumps
-from js import esprimaToBoogie
-from datetime import datetime
-
 import json
 
 Base = declarative_base();
@@ -23,55 +19,3 @@ class Event(Base):
   addr = Column(String) # For mturk users this is the ip of the access
   time = Column(DateTime)
   payload = Column(String(16536))
-
-if __name__ == "__main__":
-  import sys;
-
-  engine = create_engine('sqlite:///test.db', echo=True)
-  Session = sessionmaker(bind=engine)
-
-  s = Session()
-
-  Base.metadata.create_all(engine);
-
-  srcs = {
-    s.name : s for s in s.query(Source).all()
-  }
-
-  with open(sys.argv[1], 'r') as f:
-    for l in f:
-      e = json.loads(l)
-
-      if e["method"] != "logEvent":
-        continue;
-
-      time = e["time"]
-      ip = e['ip']
-      (worker_id, evt_type, rest) = e["args"]
-
-      if worker_id not in srcs:
-        src = Source(name=worker_id)
-        s.add(src);
-        srcs[worker_id] = src;
-
-      src = srcs[worker_id]
-      if (evt_type == "TutorialStart"):
-        payl = { }
-      elif (evt_type == "TutorialDone"):
-        payl = { }
-      elif (evt_type == "StartLevel" or evt_type == "FinishLevel"):
-        payl = { "lvlset" : rest[0], "lvlid" : rest[1] }
-        if evt_type == "FinishLevel":
-          payl["verified"] = rest[2]
-          invs = zip(rest[3], [ str(esprimaToBoogie(x, {})) for x in rest[4] ])
-          payl["all_found"] = invs;
-      elif (evt_type == "FoundInvariant"):
-        payl = [ rest[2], str(esprimaToBoogie(rest[3], { })) ]
-      else:
-        print "Unknown event: ", e
-  
-      t = datetime.fromtimestamp(time)
-      evt = Event(type=evt_type, src=src.name, addr=ip, time=t, payload=dumps(payl))
-      s.add(evt);
-
-s.commit();
