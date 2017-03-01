8339e580554190aa327acd66813e25a0af1b7eee
Dimitar Bounov <dbounov@cs.ucsd.edu>
2017-02-28 17:13:00 -0800

    Add forward dataflow analysis and sp propagation

--- src/lib/boogie/analysis.py
+++ src/lib/boogie/analysis.py
@@ -2,6 +2,30 @@ from copy import copy
 from bb import *
 from ast import *
 
+#TODO: Need to introduce a separation between top and bottom
+def forwarddflow(bbs, transformer_m, union_f, initial_vals, start_val):
+    state = { bb: copy(initial_vals) for bb in bbs.keys() }
+    state[entry(bbs)] = start_val
+    wlist = [ entry(bbs) ]
+    while len(wlist) > 0:
+        curBB = wlist.pop()
+
+        if (len(bbs[curBB].predecessors) > 0):
+          inS = union_f([state[bb] for bb in bbs[curBB].predecessors])
+        else:
+          inS = start_val
+
+        for stmt in bbs[curBB].stmts:
+            ninS = transformer_m[stmt.__class__](stmt, inS)
+            inS = ninS
+        outS = inS
+        if state[curBB] != outS:
+            wlist.extend(bbs[curBB].successors)
+            state[curBB] = outS
+    return state
+
+# TODO: How does this work without one node starting with less than maximal?
+# What is Top and Bottom in my current use caes (livevars)?
 def backdflow(bbs, transformer_m, union_f, initial_vals):
     state = { bb: copy(initial_vals) for bb in bbs.keys() }
     wlist = [ exit(bbs) ]
@@ -26,7 +50,30 @@ def livevars(bbs):
     def union_f(sets):
         return reduce(lambda x,y:   x.union(y), filter(lambda x:  x != None, sets), set([]))
     return backdflow(bbs, transformer_m, union_f, None)
-    
+
+def propagate_sp(bbs):
+    # Propagate sps involving variable not changed through the loop
+    def untouched(preds, clobbered_vars):
+      return set([x for x in preds if len(clobbered_vars.intersection(expr_read(x))) == 0])
+
+    def assignment_preds(stmt):
+      return set([AstBinExpr(stmt.lhs, "==", stmt.rhs)]) \
+        if str(stmt.lhs) not in expr_read(stmt.rhs) else set()
+
+    transformer_m = {
+        AstAssert:  lambda stmt, inS: inS,
+        AstAssume:  lambda stmt, inS: inS.union(set([stmt.expr])),
+        AstHavoc:  lambda stmt, inS: untouched(inS, stmt_changed(stmt)),
+        AstAssignment:  lambda stmt, inS:   untouched(inS, set([str(stmt.lhs)])).union(assignment_preds(stmt))
+    }
+
+    def union_f(sets):
+        something_sets = filter(lambda x:  x != None, sets)
+        if (len(something_sets)) == 0:  return None
+        return reduce(lambda x,y:   x.intersection(y), something_sets[1:], something_sets[0])
+
+    return forwarddflow(bbs, transformer_m, union_f, None, set([]))
+
 if __name__ == "__main__":
     bbs = get_bbs("desugared3_no_inv.bpl")
     print livevars(bbs)
