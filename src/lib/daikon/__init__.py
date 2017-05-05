3ba59ba900359047d95cbafd58f6c247e3b4d715
Dimo <lami4ka@gmail.com>
2017-05-04 17:08:14 -0700

    clean lint in lib/daikon

--- src/lib/daikon/__init__.py
+++ src/lib/daikon/__init__.py
@@ -1,13 +1,13 @@
 from tempfile import NamedTemporaryFile
 from subprocess import call, check_output
-from inv_ast import parseExprAst
+from lib.daikon.inv_ast import parseExprAst
 from os.path import basename
 
-def toDaikonTrace(vars, trace):
+def toDaikonTrace(varz, trace):
   r = "decl-version 2.0\n"
   r += "ppt LoopEntry:::\n"
   r += "ppt-type point\n"
-  for v in vars:
+  for v in varz:
     r += "  variable " + v + "\n";
     r += "    var-kind variable\n";
     r += "    dec-type int\n";
@@ -16,40 +16,54 @@ def toDaikonTrace(vars, trace):
   r += "\n";
   for row in trace:
     r += "LoopEntry:::\n";
-    for (var,val) in zip(vars, row):
+    for (var,val) in zip(varz, row):
       r += var + "\n" + str(val) + "\n1\n"
     r += "\n"
   return r;
 
-def runDaikon(vars, trace, nosuppress=False):
+def runDaikon(varz, trace, nosuppress=False):
   with NamedTemporaryFile(suffix=".dtrace", delete=False) as dtraceF:
-    dtraceF.write(toDaikonTrace(vars,trace));
+    dtraceF.write(toDaikonTrace(varz,trace));
     dtraceF.flush();
     args = ["java", "daikon.Daikon"]
     if (nosuppress):
-      args = args + [ "--config_option", "daikon.inv.filter.ObviousFilter.enabled=false",\
-                      "--config_option", "daikon.inv.filter.OnlyConstantVariablesFilter.enabled=false",\
-                      "--config_option", "daikon.inv.filter.ParentFilter.enabled=false",\
-                      "--config_option", "daikon.inv.filter.SimplifyFilter.enabled=false",\
-                      "--config_option", "daikon.inv.filter.UnjustifiedFilter.enabled=false",\
-                      "--config_option", "daikon.inv.unary.scalar.CompleteOneOfScalar.enabled=true",\
-                      "--config_option", "daikon.inv.unary.sequence.EltRangeInt.Even.enabled=true",\
-                      "--config_option", "daikon.inv.unary.scalar.Modulus.enabled=true",\
-#                      "--config_option", "daikon.inv.unary.scalar.NonModulus.enabled=true",\ Gets Printed weirdly = ? (mod ?)
-                      "--config_option", "daikon.inv.binary.twoScalar.NumericInt.ZeroTrack.enabled=true",\
-                      "--config_option", "daikon.inv.unary.scalar.RangeInt.Even.enabled=true",\
-                    ]
+      args = args + \
+        [ "--config_option",\
+         "daikon.inv.filter.ObviousFilter.enabled=false",\
+          "--config_option",\
+         "daikon.inv.filter.OnlyConstantVariablesFilter.enabled=false",\
+          "--config_option",\
+         "daikon.inv.filter.ParentFilter.enabled=false",\
+          "--config_option",\
+         "daikon.inv.filter.SimplifyFilter.enabled=false",\
+          "--config_option",\
+         "daikon.inv.filter.UnjustifiedFilter.enabled=false",\
+          "--config_option",\
+         "daikon.inv.unary.scalar.CompleteOneOfScalar.enabled=true",\
+          "--config_option",\
+         "daikon.inv.unary.sequence.EltRangeInt.Even.enabled=true",\
+          "--config_option",\
+         "daikon.inv.unary.scalar.Modulus.enabled=true",\
+#          "--config_option",\
+#         "daikon.inv.unary.scalar.NonModulus.enabled=true",
+          "--config_option",\
+         "daikon.inv.binary.twoScalar.NumericInt.ZeroTrack.enabled=true",\
+          "--config_option",\
+         "daikon.inv.unary.scalar.RangeInt.Even.enabled=true",\
+        ]
     args.append(dtraceF.name)
     raw = check_output(args)
     call(["rm", basename(dtraceF.name)[:-len(".dtrace")]+".inv.gz"])
     start = raw.index("LoopEntry:::") + len("LoopEntry:::")
     end = raw.index("Exiting Daikon.", start)
-    invs = filter(lambda x: x != "", map(lambda x:  x.strip(), raw[start:end].split("\n")))
-    # I don't understand how LinearTeranry invariants without justification are displayed...
-    invs = filter(lambda x: "warning: too few samples for daikon.inv.ternary.threeScalar.LinearTernary invariant" not in x, invs);
+    invs = filter(lambda x: x != "",
+                  map(lambda x:  x.strip(), raw[start:end].split("\n")))
+    # I don't understand how LinearTeranry invariants without justification are
+    # displayed... Also getting lazy to fix the long line..
+    invs = filter(lambda x: "warning: too few samples for daikon.inv.ternary.threeScalar.LinearTernary invariant" not in x, invs); #pylint: disable=line-too-long
     invs = filter(lambda x: "(mod 0)" not in x, invs);
     try:
-      return map(lambda x:  parseExprAst(x), invs)
+      return map(parseExprAst, invs)
     except:
       print invs;
       raise;
--- src/lib/daikon/inv_ast.py
+++ src/lib/daikon/inv_ast.py
@@ -1,57 +1,86 @@
-from inv_grammar import *
+#pylint: disable=no-self-argument, unused-argument
+from lib.inv_grammar import DaikonInvParser
 from ..common.ast import AstNode as AstNode
 
 class AstUnExpr(AstNode):
-    def __init__(s, op, expr):  AstNode.__init__(s, str(op), expr)
-    def __str__(s): return s.op + str(s.expr)
+    def __init__(s, op, expr):
+         AstNode.__init__(s, str(op), expr)
+    def __str__(s):
+        return s.op + str(s.expr)
 
 class AstIsPow2(AstNode):
-    def __init__(s, expr):  AstNode.__init__(s, expr)
-    def __str__(s): return "IsPow2(" + str(s.expr) + ")"
+    def __init__(s, expr):
+         AstNode.__init__(s, expr)
+    def __str__(s):
+        return "IsPow2(" + str(s.expr) + ")"
 
 class AstIsOneOf(AstNode):
-    def __init__(s, expr, options):  AstNode.__init__(s, expr, options)
-    def __str__(s): return "IsOneOf(" + str(s.expr) + ",[" + ",".join(map(str, s.options)) + "])"
+    def __init__(s, expr, options):
+         AstNode.__init__(s, expr, options)
+    def __str__(s):
+        return "IsOneOf(" + str(s.expr) + \
+                ",[" + ",".join(map(str, s.options)) + "])"
 
 class AstIsBoolean(AstNode):
-    def __init__(s, expr):  AstNode.__init__(s, expr)
-    def __str__(s): return "IsBoolean(" + str(s.expr) + ")"
+    def __init__(s, expr):
+         AstNode.__init__(s, expr)
+    def __str__(s):
+        return "IsBoolean(" + str(s.expr) + ")"
 
 class AstIsEven(AstNode):
-    def __init__(s, expr):  AstNode.__init__(s, expr)
-    def __str__(s): return "IsEven(" + str(s.expr) + ")"
+    def __init__(s, expr):
+         AstNode.__init__(s, expr)
+    def __str__(s):
+        return "IsEven(" + str(s.expr) + ")"
 
 class AstInRange(AstNode):
-    def __init__(s, lower, expr, upper):  AstNode.__init__(s, lower, expr, upper)
-    def __str__(s): return str(s.expr) + " in [" + str(s.lower) +  "," + str(s.upper) +  "]"
+    def __init__(s, lower, expr, upper):
+         AstNode.__init__(s, lower, expr, upper)
+    def __str__(s):
+        return str(s.expr) + " in [" + str(s.lower) +  "," + str(s.upper) +  "]"
 
 class AstIsConstMod(AstNode):
-    def __init__(s, expr, remainder, modulo):  AstNode.__init__(s, expr, remainder, modulo)
-    def __str__(s): return "IsConstMod(" + str(s.expr) + "," + str(s.remainder) + "," + str(s.modulo) + ")"
+    def __init__(s, expr, remainder, modulo):
+         AstNode.__init__(s, expr, remainder, modulo)
+    def __str__(s):
+        return "IsConstMod(" + str(s.expr) + "," + str(s.remainder) + \
+                "," + str(s.modulo) + ")"
 
 class AstHasValues(AstNode):
-    def __init__(s, expr, values):  AstNode.__init__(s, expr, values)
-    def __str__(s): return "HasValues(" + str(s.expr) + "," + str(s.values) + ")"
+    def __init__(s, expr, values):
+         AstNode.__init__(s, expr, values)
+    def __str__(s):
+        return "HasValues(" + str(s.expr) + "," + str(s.values) + ")"
 
-class AstFalse(AstNode): 
-    def __init__(s):  AstNode.__init__(s)
-    def __str__(s): return "false"
+class AstFalse(AstNode):
+    def __init__(s):
+         AstNode.__init__(s)
+    def __str__(s):
+        return "false"
 
 class AstTrue(AstNode):
-    def __init__(s):  AstNode.__init__(s)
-    def __str__(s): return "true"
+    def __init__(s):
+         AstNode.__init__(s)
+    def __str__(s):
+        return "true"
 
-class AstNumber(AstNode): 
-    def __init__(s, num):   AstNode.__init__(s,int(num))
-    def __str__(s): return str(s.num)
+class AstNumber(AstNode):
+    def __init__(s, num):
+          AstNode.__init__(s,int(num))
+    def __str__(s):
+        return str(s.num)
 
-class AstId(AstNode): 
-    def __init__(s, name):  AstNode.__init__(s, str(name))
-    def __str__(s): return s.name
+class AstId(AstNode):
+    def __init__(s, name):
+         AstNode.__init__(s, str(name))
+    def __str__(s):
+        return s.name
 
 class AstBinExpr(AstNode):
-    def __init__(s, lhs, op, rhs):  AstNode.__init__(s, lhs, str(op), rhs)
-    def __str__(s): return "(" + str(s.lhs) + " " + s.op + " " + str(s.rhs) + ")"
+    def __init__(s, lhs, op, rhs):
+         AstNode.__init__(s, lhs, str(op), rhs)
+    def __str__(s):
+        return "(" + str(s.lhs) + " " + s.op + " " + str(s.rhs) + ")"
 
 class AstBuilder(DaikonInvParser):
   def onAtom(s, prod, st, loc, toks):
@@ -74,7 +103,9 @@ class AstBuilder(DaikonInvParser):
       assert(len(toks) > 3);
       base = AstBinExpr(*toks[:3])
       rest = [ [toks[3+2*k], toks[3+2*k+1]] for k in xrange((len(toks)-3)/2) ]
-      return [ reduce(lambda acc,el:  AstBinExpr(acc, el[0], el[1]), rest, base) ]
+      return [ reduce(lambda acc,el:  AstBinExpr(acc, el[0], el[1]),
+                      rest,
+                      base) ]
 
   def onRABinOp(s, prod, st, loc, toks):
     if (len(toks) == 3):
@@ -83,8 +114,9 @@ class AstBuilder(DaikonInvParser):
       assert(len(toks) > 3);
       toks = reversed(toks)
       base = AstBinExpr(*toks[:3])
-      rest = [ [toks[3+2*k], toks[3+2*k+1]] for k in xrange((len(toks)-3)/2) ]
-      return [ reduce(lambda acc,el:  AstBinExpr(acc, el[0], el[1]), toks[3:], base) ]
+      return [ reduce(lambda acc,el:  AstBinExpr(acc, el[0], el[1]),
+                      toks[3:],
+                      base) ]
 
   def onNABinOp(s, prod, st, loc, toks):
     if (prod == s.IsInRange):
