1ca1f2d3bc2183aa5f4ff6d2e01f6cd360e2f58b
Anthony DeRossi <aderossi@cs.ucsd.edu>
2017-08-09 17:45:42 -0400

    Add lvlset/additional invariant loading to verify.py
    If a levelset is specified with the --lvlset option then each level in
    the levelset will be verified, automatically filtering invariants using
    the level name.
    
    A file containing additional invariants in CSV format (level name and a
    semicolon-separated list of invariants) can be specified using the
    --additionalInvs option.  Additional invariants will be included
    automatically when verifying all levels in a levelset.

--- src/verify.py
+++ src/verify.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python2.7
 
 from argparse import ArgumentParser
+import csv
 
 from db_util import allInvs
-from levels import loadBoogieFile
+from levels import loadBoogieFile, loadBoogieLvlSet
 from lib.boogie.ast import parseExprAst
+from lib.boogie.z3_embed import AllIntTypeEnv, Unknown, expr_to_z3, tautology
 from models import open_sqlite_db, open_mysql_db
 from vc_check import tryAndVerifyLvl
 
@@ -19,11 +21,44 @@ def verify(lvl, invs, timeout=None):
   print
   print "SOLVED:", not len(violations)
 
+def processLevel(args, lvl, lvlName=None, additionalInvs=[]):
+  lvls = args.lvls if lvlName is None else [lvlName]
+
+  actualEnames = set()
+  actualLvls = set()
+  actualLvlsets = set()
+  actualWorkers = set()
+  dbInvs = allInvs(s, args.enames, lvls, args.lvlsets, args.workers,
+    actualEnames, actualLvls, actualLvlsets, actualWorkers)
+
+  invs = set(parseExprAst(inv[0]) for inv in dbInvs)
+  invs.update(additionalInvs)
+
+  if lvlName is not None:
+    print "_" * 40
+    print "RESULTS FOR LEVEL", lvlName
+    print
+  print "ENAMES:", ", ".join(actualEnames)
+  print "LVLS:", ", ".join(actualLvls)
+  print "LVLSETS:", ", ".join(actualLvlsets)
+  if len(actualWorkers) < 6:
+    print "WORKERS:", ", ".join(actualWorkers)
+  else:
+    print "UNIQUE WORKERS:", len(actualWorkers)
+  if len(additionalInvs) < 6:
+    print "ADDITIONAL INVARIANTS:", ", ".join(additionalInvs)
+  else:
+    print "ADDITIONAL INVARIANTS:", len(additionalInvs)
+  print "UNIQUE INVARIANTS:", len(invs)
+  print
+
+  verify(lvl, invs, args.timeout)
+
 if __name__ == "__main__":
   p = ArgumentParser(description="Verify a level using selected invariants")
   p.add_argument("--additionalInvs",
     help="A .csv file with additional invariants")
-  p.add_argument("--bpl", required=True,
+  p.add_argument("--bpl",
     help="A desugared .bpl file for the level to verify")
   p.add_argument("--db", required=True,
     help="The event database to use")
@@ -31,6 +66,8 @@ if __name__ == "__main__":
     help="Include experiment invs; may specify multiple items (unset = any)")
   p.add_argument("--lvls", nargs="*",
     help="Include level invs; may specify multiple items (unset = any)")
+  p.add_argument("--lvlset",
+    help="A levelset file to load levels from")
   p.add_argument("--lvlsets", nargs="*",
     help="Include lvlset invs; may specify multiple items (unset = any)")
   p.add_argument("--timeout", type=int, default=30,
@@ -46,33 +83,46 @@ if __name__ == "__main__":
     sessionF = open_sqlite_db(args.db)
   s = sessionF()
 
-  try:
-    lvl = loadBoogieFile(args.bpl, False)
-  except Exception as e:
-    print "Couldn't load boogie file--is it desugared?"
-    raise e
-
-  if args.additionalInvs:
-    print "Additional invariants not yet implemented" # TODO
+  if args.bpl:
+    try:
+      lvl = loadBoogieFile(args.bpl, False)
+    except Exception as e:
+      print "Couldn't load boogie file--is it desugared?"
+      raise e
+  elif args.lvlset:
+    lvlset, lvls = loadBoogieLvlSet(args.lvlset)
+  else:
+    print "Either --bpl or --lvlset must be specified"
     exit(1)
 
-  actualEnames = set()
-  actualLvls = set()
-  actualLvlsets = set()
-  actualWorkers = set()
-  dbInvs = allInvs(s, args.enames, args.lvls, args.lvlsets, args.workers,
-    actualEnames, actualLvls, actualLvlsets, actualWorkers)
+  additionalInvs = {}
+  if args.additionalInvs:
+    with open(args.additionalInvs) as f:
+      r = csv.reader(f, delimiter=",")
+      for row in r:
+        lvl, invlist = row
+        invs = []
+        for invstr in invlist.split(";"):
+          if not len(invstr.strip()):
+            continuer
+          try:
+            inv = parseExprAst(invstr)
+            if tautology(expr_to_z3(inv, AllIntTypeEnv())):
+              continue
+          except RuntimeError:
+            # Some invariants are too large to parse
+            continue
+          except Unknown:
+            # Timeouts could be valid invariants
+            pass
+          invs.append(inv)
+        additionalInvs[lvl] = invs
 
-  invs = set(parseExprAst(inv[0]) for inv in dbInvs)
+  print "ADDITIONAL INVARIANTS LOADED FOR LVLS:", \
+    ", ".join(additionalInvs.keys())
 
-  print "ENAMES:", ", ".join(actualEnames)
-  print "LVLS:", ", ".join(actualLvls)
-  print "LVLSETS:", ", ".join(actualLvlsets)
-  if len(actualWorkers) < 6:
-    print "WORKERS:", ", ".join(actualWorkers)
-  else:
-    print "UNIQUE WORKERS:", len(actualWorkers)
-  print "UNIQUE INVARIANTS:", len(invs)
-  print
-
-  verify(lvl, invs, args.timeout)
+  if args.bpl:
+    processLevel(args, lvl)
+  elif args.lvlset:
+    for lvlName, lvl in lvls.items():
+      processLevel(args, lvl, lvlName, additionalInvs.get(lvlName, []))
