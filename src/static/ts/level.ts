43ead232b8969e4b273b1971315b0c9b7afab8a8
Anthony DeRossi <aderossi@cs.ucsd.edu>
2017-08-10 14:05:42 -0400

    Remove PrepopulatedDynamicLevel
    This code isn't used and makes it difficult to pass starting invariants.

--- src/static/ts/level.ts
+++ src/static/ts/level.ts
@@ -29,46 +29,15 @@ class DynamicLevel extends Level{
       cb(new DynamicLevel(id, data.variables, data.data, data.goal, data.hint, data.colSwap, data.exploration_state))
     })
   }
-}
-
-class PrepopulatedDynamicLevel extends DynamicLevel {
-  constructor(public id: string,
-    variables: string[],
-    data:  dataT,
-    goal:  any,
-    hint:  any,
-    colSwap: any,
-    exploration_state: any,
-    public startingInvs: [[invariantT, any[]][], [invariantT, [any[], any[]]][], invariantT[]]) {
-    super(id, variables, data, goal, hint, colSwap, exploration_state);
-  }
-
-  static load(lvlSet: string, id: string, cb: (lvl: Level)=>void) {
-    rpc_loadLvlDynamic(lvlSet, id, function(data) {
-      cb(new PrepopulatedDynamicLevel(id, data.variables, data.data, data.goal, data.hint, data.colSwap, data.exploration_state, [[], [], []]))
-    })
-  }
-
-  genNext(lvlSet:string, invs: invariantT[], cb:(lvl: [string, Level])=>void) {
-    rpc_genNextLvlDynamic(Args.get_worker_id(), lvlSet, this.id, invs, (data: loadNextLvlDynamicRes) => {
-      if (data == null) {
-        cb(null);
-      } else {
-        let lvl = new PrepopulatedDynamicLevel(data.id, data.variables, data.data, data.goal, data.hint, data.colSwap, data.exploration_state, [[], [], []]);
-        cb([lvlSet, lvl]);
-      }
-    });
-  }
 
   static loadNext(cb: (res: [string, Level])=>void) {
     rpc_loadNextLvlDynamic(Args.get_worker_id(), function(data) {
       if (data === null)
         cb(null)
       else {
-        let lvl = new PrepopulatedDynamicLevel(data.id, data.variables, data.data, data.goal, data.hint, data.colSwap, data.exploration_state, [[], [], []]);
+        let lvl = new DynamicLevel(data.id, data.variables, data.data, data.goal, data.hint, data.colSwap, data.exploration_state);
         cb([data.lvlSet, lvl]);
       }
     })
   }
-
 }
