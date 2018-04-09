01871decd1458efafd9c43c957a8883d34349185
Sorin Lerner <lerner@cs.ucsd.edu>
2018-04-09 14:29:10 -0700

    updated tutorial to take into account changes for text box input

--- src/static/ts/tutorial_game.ts
+++ src/static/ts/tutorial_game.ts
@@ -1,39 +1,28 @@
 import {rpc_loadLvlBasic, rpc_checkSoundness} from "./rpc";
-import {Fun, BB} from "./boogie";
+import {Fun, BB, Expr_T} from "./boogie";
 import * as Phaser from "phaser-ce"
-import {Node, AssumeNode, buildGraph, removeEmptyNodes, UserNode, AssignNode, AssertNode} from "./game_graph"
-import {InvGraphGame, TraceLayout, Trace, InvNetwork, InputOutputIcon} from "./invgraph_game"
-import {assert, getUid} from "./util"
+import {Node, AssumeNode, AssertNode, AssignNode, buildGraph, removeEmptyNodes, UserNode} from "./game_graph"
+import {InvGraphGame, TraceLayout, Trace, InvNetwork, InputOutputIcon, Violation} from "./invgraph_game"
+import {getUid, assert, repeat, structEq, StrMap} from "./util"
 import {parse} from "esprima"
 import {Point} from "phaser-ce";
+import { TextIcon } from "ts/texticon";
 
 class TutorialGame extends InvGraphGame {
   unselect(): void {
-    super.unselect();
     $("#inv").val("");
     $("#inv").prop("disabled", true);
     $("#overlay").prop("display", "none");
   }
 
   select(n: UserNode):void {
-    super.select(n);
-    $("#inv").val(n.expr);
+    $("#inv").val(n.exprs.join("&&"));
     $("#inv").prop("disabled", false);
     $("#overlay").prop("display", "none");
   }
 
   create(): void {
     super.create();
-    let invBox = $("#inv")
-    invBox.keyup((e) => {
-      if(e.keyCode == 13)
-      {
-        this.userInput(invBox.val());
-      } else if (e.keyCode == 27) {
-        this.unselect();
-      }
-    })
-
     $("#step").on("click", (e) => { this.stepForward();})
     $("#back").on("click", (e) => { this.stepBackwards();})
     $("#play").on("click", (e) => {
@@ -45,6 +34,40 @@ class TutorialGame extends InvGraphGame {
       this._controlsStopped();
     })
     this._controlsDisable();
+    this.forEachUserNode((nd: UserNode) => {
+      this.textSprites[nd.id].onChanged.add((gameEl: TextIcon, newLines: string[])=> {
+        assert (newLines.length >= nd.sound.length)
+        let soundLines = newLines.slice(newLines.length-nd.sound.length);
+        let unknownLines = newLines.slice(0, newLines.length-nd.sound.length);
+        // newLines must have nd.unsound as its suffix (since those are immutable)
+        assert(structEq(soundLines, nd.sound))
+
+        unknownLines = unknownLines.filter((ln: string) => ln.trim().length != 0);
+
+        for (let i = 0; i < unknownLines.length; i++) {
+          try {
+            parse(unknownLines[i]);
+          } catch (err) {
+            // Couldn't parse i-th line
+            console.log("Error parsing: " + err);
+            gameEl.edit(i);
+            return;
+          }
+        }
+        nd.sound = soundLines; 
+        nd.unsound = unknownLines;
+
+        let invNet: InvNetwork = this.getInvNetwork();
+        this.checkInvs(invNet, ()=> {});
+      })
+    })
+  }
+
+  forEachUserNode(cb: (nd: UserNode) => any): void {
+    this.entry.forEachReachable((nd: Node) => {
+      if (!(nd instanceof UserNode))  return;
+      cb(nd);
+    })
   }
 
   _controlsPlaying(): void {
@@ -91,37 +114,37 @@ class TutorialGame extends InvGraphGame {
     let v = [];
     v[0] = ["anon0"];
     v[1] = [[{n:1}, {n:1}, {n:2}, {n:2}]];
-    this.setViolations([v], onDone);
-    // rpc_checkSoundness("unsolved-new-benchmarks2", this.lvlId, invs, (res) => {
-    //   this.setViolations(res, () => {
-    //     this.transformLayout(() => {
-    //       if (this.selected !== null) {
-    //         this.nodeSprites[this.selected.id] = this.drawNode(this.selected, new Point(800, 600), 15)
-    //       }
-    //     }, onDone);
-    //   });
-    // });
+    let res = [v];
+    this.setViolations(res, () => {
+      this.transformLayout(() => {
+        let soundnessViolations: StrMap<Violation[]> = {};
+        for (let v of res) {
+          let t: Trace = this.getTrace(v);
+          let end: UserNode = (t[t.length-1][0] instanceof UserNode ? t[t.length-1][0] as UserNode: null);
+          if (end == null) continue;
+          if (!(end.id in soundnessViolations)) {
+            soundnessViolations[end.id] = [];
+          }
+          soundnessViolations[end.id].push(v);
+        }
+
+        this.forEachUserNode((un: UserNode) => {
+          if (!(un.id in soundnessViolations) || 
+              soundnessViolations[un.id].length == 0) {
+            un.sound = un.unsound.concat(un.sound);
+            un.unsound = [];
+          }
+          let spriteNode = this.nodeSprites[un.id] as InputOutputIcon;
+          spriteNode.setInvariants(un.sound, un.unsound);
+        })
+      }, onDone);
+    });
   }
 
   onFirstUpdate(): void {
     super.onFirstUpdate();
     this.checkInvs(this.getInvNetwork(), ()=>{});
   }
-
-  userInput: any = (inv: string) => {
-    assert(this.selected != null);
-    try {
-      let pinv = parse(inv);
-    } catch (e) {
-      console.log("Couldn't parse");
-      return
-    }
-
-    this.selected.expr = inv;
-    let invNet: InvNetwork = this.getInvNetwork();
-    this.checkInvs(invNet, ()=> {});
-  }
-
 }
 
 $(document).ready(function() {
