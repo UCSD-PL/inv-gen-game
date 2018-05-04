9ac8354e3aab687cc71dbb4d66ca014fd8c556d6
Massimiliano Menarini <mmenarini@UCSD.EDU>
2018-05-03 17:08:51 -0700

    Add parameters to configure system

--- init.sh
+++ init.sh
@@ -3,12 +3,11 @@
 set -e
 
 
-
 echo "Starting SSH ..."
-
 service ssh start
 
 
-
+cd /app/src
 source ../env/bin/activate
-python3 server.py --local --log useractions.log --lvlset ../lvlsets/unsolved-leftover-split.lvlset --db mysql+pymysql://gamification:m58xXuH5VBuWXtaW@gamificationfacebook.cpepc4fuqyhq.us-west-2.rds.amazonaws.com/game --adminToken fuzzy --timeout 60
+python3 server.py --local --log useractions.log --lvlset $LEVEL_SET --db mysql+pymysql://$SQL_CONNECTION --adminToken $ADMIN_TOKEN --timeout 60
+#python3 server.py --local --log useractions.log --lvlset ../lvlsets/unsolved-leftover-split.lvlset --db mysql+pymysql://gamification:m58xXuH5VBuWXtaW@gamificationfacebook.cpepc4fuqyhq.us-west-2.rds.amazonaws.com/game --adminToken fuzzy --timeout 60
