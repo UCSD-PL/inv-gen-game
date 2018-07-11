ddae23f0bc24a6bc397fc6a9579deb8d825bf4d0
Dimo <lami4ka@gmail.com>
2018-07-10 19:16:06 -0700

    Type pp.py

--- src/pp.py
+++ src/pp.py
@@ -1,4 +1,8 @@
-from js import esprimaToZ3
+from js import esprimaToZ3, EsprimaNode
+from typing import List, TypeVar, Callable, Tuple, Any
+from db_util import MturkIdT
+
+T=TypeVar("T")
 
 def pp_BoogieLvl(lvl):
   if (lvl != None):
@@ -6,32 +10,35 @@ def pp_BoogieLvl(lvl):
   else:
     return str(lvl)
 
-def pp_EsprimaInv(inv):
+def pp_EsprimaInv(inv: EsprimaNode) -> str:
   return str(esprimaToZ3(inv, {}))
 
-def pp_List(l, pp_el):
+def pp_List(l: List[T], pp_el: Callable[[T], str]) -> str:
   return "[" + ",".join([pp_el(el) for el in l]) + "]"
 
-def pp_Tuple(t, *pps):
+def pp_Tuple(t: Tuple[Any,...], *pps: Callable[[Any], str]):
   return "(" + ",".join([pp(el) for (pp,el) in zip(pps, t)]) + ")"
 
-def pp_EsprimaInvs(invs):
+def pp_EsprimaInvs(invs: List[EsprimaNode]) -> str:
   return pp_List(invs, pp_EsprimaInv)
 
-def pp_EsprimaInvPairs(invPairs):
+def pp_EsprimaInvPairs(invPairs: List[Tuple[EsprimaNode, EsprimaNode]]) -> str:
   return pp_List(invPairs, lambda p:  pp_Tuple(p, pp_EsprimaInv, pp_EsprimaInv))
 
-def pp_Ctrex(ctrx):
+# TODO: Make more specific than Any
+def pp_Ctrex(ctrx: Tuple[EsprimaNode, Any]) -> str:
   return pp_Tuple(ctrx, pp_EsprimaInv, str)
 
-def pp_Ctrexs(ctrxs):
+# TODO: Make more specific than Any
+def pp_Ctrexs(ctrxs: List[Any]) -> str:
   return pp_List(ctrxs, pp_Ctrex);
 
-def pp_CheckInvsRes(res):
+# TODO: Make more specific than Any
+def pp_CheckInvsRes(res: Any) -> str:
   return pp_Tuple(res, pp_Ctrexs, pp_Ctrexs, pp_EsprimaInvs)
 
-def pp_tryAndVerifyRes(res):
+def pp_tryAndVerifyRes(res: Any) -> str:
   return pp_Tuple(res, pp_Ctrexs, pp_Ctrexs, pp_EsprimaInvs, str, str)
 
-def pp_mturkId(mId):
+def pp_mturkId(mId: MturkIdT) -> str:
   return pp_Tuple(mId, str, str, str)
