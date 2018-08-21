66c0cb296fb33a7eee29756fbc69a9b0c1713813
Dimo <lami4ka@gmail.com>
2018-08-20 22:41:30 -0700

    Make traceWindow work on detached dom trees; Kill old 2-player trace window

--- src/frontend/ts/traceWindow.ts
+++ src/frontend/ts/traceWindow.ts
@@ -48,7 +48,7 @@ export abstract class BaseTracesWindow implements ITracesWindow {
     }
 
     immediateError(msg: string): void {
-        $("th #errormsg").html("<div class='error'> " + msg + "</div>");
+        $("th #errormsg", this.parent).html("<div class='error'> " + msg + "</div>");
         this.reflowCb();
     }
 
@@ -58,7 +58,7 @@ export abstract class BaseTracesWindow implements ITracesWindow {
     }
 
     immediateMsg(msg: string): void {
-        $("th #errormsg").html("<div class='msg'> " + msg + "</div>");
+        $("th #errormsg", this.parent).html("<div class='msg'> " + msg + "</div>");
         this.reflowCb();
     }
 
@@ -81,9 +81,9 @@ export abstract class BaseTracesWindow implements ITracesWindow {
         hstr += '<th><input id="formula-entry" tabindex="-1" type="text"><span id="errormsg"><div>&nbsp;</div></span></th>';
         hstr += '</tr></thead><tbody></tbody></table>';
         $(this.parent).html(hstr);
-        $('#formula-entry').focus();
+        $('#formula-entry', this.parent).focus();
         var tW = this;
-        $('#formula-entry').keyup(function (keyEv) {
+        $('#formula-entry', this.parent).keyup(function (keyEv) {
             let curInv = invPP(tW.curExp());
             if (keyEv.keyCode == 13 && tW.okToSubmit) {
                 tW.commitCb();
@@ -102,13 +102,13 @@ export abstract class BaseTracesWindow implements ITracesWindow {
     abstract evalResult(res: resT): void;
 
     curExp(): string {
-        let v = $("#formula-entry").val();
+        let v = $("#formula-entry", this.parent).val();
         return (<string>v === undefined ? "" : <string>v);
     }
 
     setExp(exp: string) {
-        $("#formula-entry").val(exp);
-        $("#formula-entry").focus();
+        $("#formula-entry", this.parent).val(exp);
+        $("#formula-entry", this.parent).focus();
         this.changedCb();
     }
 
@@ -122,22 +122,22 @@ export abstract class BaseTracesWindow implements ITracesWindow {
 
     disableSubmit(): void { this.okToSubmit = false; }
 
-    disable(): void { $("#formula-entry").attr("disabled", "disabled"); }
+    disable(): void { $("#formula-entry", this.parent).attr("disabled", "disabled"); }
 
-    enable(): void { $("#formula-entry").removeAttr("disabled"); }
+    enable(): void { $("#formula-entry", this.parent).removeAttr("disabled"); }
 
     highlightRect(x: number, y: number, w: number, h: number, border_style: string, backgroud_style: string) {
         for (let i = x; i < x + w; i++) {
-            $("#" + y + "_" + i).css('border-top', border_style);
-            $("#" + (y + h - 1) + "_" + i).css('border-bottom', border_style);
+            $("#" + y + "_" + i, this.parent).css('border-top', border_style);
+            $("#" + (y + h - 1) + "_" + i, this.parent).css('border-bottom', border_style);
         }
         for (let j = y; j < y + h; j++) {
-            $("#" + j + "_" + x).css('border-left', border_style);
-            $("#" + j + "_" + (x + w - 1)).css('border-right', border_style);
+            $("#" + j + "_" + x, this.parent).css('border-left', border_style);
+            $("#" + j + "_" + (x + w - 1), this.parent).css('border-right', border_style);
         }
         for (let i = x; i < x + w; i++) {
             for (let j = y; j < y + h; j++) {
-                $("#" + j + "_" + i).css('background-color', backgroud_style);
+                $("#" + j + "_" + i, this.parent).css('background-color', backgroud_style);
             }
         }
     }
@@ -184,7 +184,7 @@ export class PositiveTracesWindow extends BaseTracesWindow {
         // For now support a single inductive counterexample
         assert(data[1].length === 0 && data[2].length === 0);
         let classes = ["true", "false", "inductive"];
-        let id = $("table#lvl_table tr.traces-row").length;
+        let id = $("table#lvl_table tr.traces-row", this.parent).length;
         let lbls = [];
 
         for (let i in data[0]) {
@@ -197,7 +197,7 @@ export class PositiveTracesWindow extends BaseTracesWindow {
                 "<td class='temp_expr_eval'>&nbsp</td>" +
                 "</tr>");
             this.data[0].push(data[0][i]);
-            $("table#lvl_table tbody").append(curRow);
+            $("table#lvl_table tbody", this.parent).append(curRow);
             this.dataMap[0][data_id] = curRow;
             id++;
         }
@@ -216,175 +216,10 @@ export class PositiveTracesWindow extends BaseTracesWindow {
                 }
             }
         } else if (res.hasOwnProperty("clear")) {
-            $(".temp_expr_eval").html("");
-            $(".traces-row td.temp_expr_eval").removeClass("true");
-            $(".traces-row td.temp_expr_eval").removeClass("false");
+            $(".temp_expr_eval", this.parent).html("");
+            $(".traces-row td.temp_expr_eval", this.parent).removeClass("true");
+            $(".traces-row td.temp_expr_eval", this.parent).removeClass("false");
         }
         das.reflowAll();
     }
-}
-
-class TwoPlayerTracesWindow extends BaseTracesWindow {
-    player: number;
-
-    constructor(public playerNum: number, public parent: HTMLElement) {
-        super(parent);
-        this.player = playerNum;
-
-        $(this.parent).removeClass("tracesWindow");
-
-        if (this.player === 1) {
-            $(this.parent).addClass("tracesWindow1");
-        }
-        else if (this.player === 2) {
-            $(this.parent).addClass("tracesWindow2");
-        }
-    }
-
-    getPlayer(): number {
-        return this.player;
-    }
-
-    changed(cb) {
-        this.changedCb = cb;
-    }
-
-    commit(cb) {
-        this.commitCb = cb;
-    }
-
-    setVariables(lvl: Level): void {
-        let hstr = "<table id='lvl_table" + this.player + "' class='table table-stripped'><thead><tr>";
-        for (let i in lvl.variables) {
-            hstr += "<th>" + lvl.variables[i] + "</th>";
-        }
-        if (this.player === 1) {
-            hstr += "<th><input id='formula-entry' type='text'><span id='errormsg'>&nbsp</span></th>";
-        }
-        else if (this.player === 2) {
-            hstr += "<th><input id='formula-entry2' type='text'><span id='errormsg'>&nbsp</span></th>";
-        }
-
-        hstr += "</tr></thead><tbody></tbody></table>";
-        $(this.parent).html(hstr);
-
-        if (this.player === 1) {
-            $("#formula-entry").focus();
-            let tW = this;
-            $("#formula-entry").keyup(function (keyEv) {
-                let curInv = invPP(tW.curExp());
-                if (keyEv.keyCode === 13 && tW.okToSubmit) {
-                    tW.commitCb();
-                    $("#formula-entry").blur();
-                    setTimeout(function () { $("#btn-switch").click(); }, 2000);
-                    $("#formula-entry2").focus();
-                } else if (curInv !== tW.ppInv) {
-                    tW.ppInv = curInv;
-                    tW.changedCb();
-                }
-            });
-        }
-        else if (this.player === 2) {
-            $("#formula-entry2").focus();
-            let tW = this;
-            $("#formula-entry2").keyup(function (keyEv) {
-                let curInv = invPP(tW.curExp());
-                if (keyEv.keyCode === 13 && tW.okToSubmit) {
-                    tW.commitCb();
-                    $("#formula-entry2").blur();
-                    setTimeout(function () { $("#btn-switch2").click(); }, 2000);
-                    $("#formula-entry").focus();
-                } else if (curInv !== tW.ppInv) {
-                    tW.ppInv = curInv;
-                    tW.changedCb();
-                }
-            });
-        }
-
-        this.dataMap = [[], [], []];
-        this.data = [[], [], []];
-        das.reflowAll();
-    }
-
-    addData(data: dataT) {
-        assert(data[1].length === 0 && data[2].length === 0);
-        let classes = ["true", "false", "inductive"];
-        let id = $("table#lvl_table" + this.player + " tr.traces-row").length;
-        let lbls = [];
-
-        for (let i in data[0]) {
-            let data_id = this.data[0].length;
-            let curRow = $(
-                "<tr class='traces-row' id='" + id + "'>" + // this.player + id???
-                data[0][i].map(el =>
-                    "<td class='" + classes[0] + "'>" + el + "</td>").join("") +
-                "<td class='temp_expr_eval'>&nbsp</td>" +
-                "</tr>");
-            this.data[0].push(data[0][i]);
-            $("table#lvl_table" + this.player + " tbody").append(curRow);
-            this.dataMap[0][data_id] = curRow;
-            id++;
-        }
-        das.reflowAll();
-    }
-
-    evalResult(res: resT) {
-        if (res.hasOwnProperty("data")) {
-            let resD = <resDataT>res;
-
-            for (let type in resD.data) {
-                for (let i in resD.data[type]) {
-                    let datum = resD.data[type][i];
-                    let row = this.dataMap[type][i];
-                    this.setResultCell(row, datum);
-                }
-            }
-        }
-        else if (res.hasOwnProperty("clear")) {
-            $(".temp_expr_eval").html("");
-            $(".traces-row td.temp_expr_eval").removeClass("true");
-            $(".traces-row td.temp_expr_eval").removeClass("false");
-        }
-        das.reflowAll();
-    }
-
-    curExp(): string {
-        let v = null;
-        if (this.player === 1) {
-            v = $("#formula-entry").val();
-        }
-        else if (this.player === 2) {
-            v = $("#formula-entry2").val();
-        }
-        return (v === undefined ? "" : v);
-    }
-
-    setExp(exp: string) {
-        if (this.player === 1) {
-            $("#formula-entry").val(exp);
-        }
-        else if (this.player === 2) {
-            $("#formula-entry2").val(exp);
-        }
-        this.changedCb();
-    }
-
-    disable(): void {
-        if (this.player === 1) {
-            $("#formula-entry").attr("disabled", "disabled");
-        }
-        else if (this.player === 2) {
-            $("#formula-entry2").attr("disabled", "disabled");
-        }
-    }
-
-    enable(): void {
-        if (this.player === 1) {
-            $("#formula-entry").removeAttr("disabled");
-        }
-        else if (this.player === 2) {
-            $("#formula-entry2").removeAttr("disabled");
-        }
-    }
-
 }
