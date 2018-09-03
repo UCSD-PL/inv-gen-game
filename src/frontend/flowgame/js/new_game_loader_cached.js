3a0f5623768bd17b532eb95780414a55dccf9abc
Dimo <lami4ka@gmail.com>
2018-09-03 14:32:16 -0700

    Add a static version of new_game.html/new_game_loader.js for offline development

---
+++ src/frontend/flowgame/js/new_game_loader_cached.js
@@ -0,0 +1,60 @@
+requirejs.config({
+  paths: {
+    "jquery": "/game/cache/jquery-1.12.0",
+    "jquery_color": "/game/cache/jquery.color-2.1.2",
+    "jquery_ui": "/game/cache/jquery-ui",
+    "bootstrap": "/game/cache/bootstrap",
+    '../../ts/facebook': "/game/build/ts/facebook",
+    'facebook-js-sdk': "/game/cache/sdk",
+    "jsonrpcclient": "/game/js/jquery.jsonrpcclient",
+    "esprima": '/game/js/esprima',
+    "phaser-ce": "/game/flowgame/js/phaser",
+    "bonus": "/game/build/ts/bonus",
+    "container": "/game/build/ts/container",
+    "ctrexGameLogic": "/game/build/ts/ctrexGameLogic",
+    "ctrexTracesWindow": "/game/build/ts/ctrexTracesWindow",
+    "curvedarrow": "/game/build/ts/curvedarrow",
+    "../../ts/eval": "/game/build/ts/eval",
+    "gameLogic": "/game/build/ts/gameLogic",
+    "../../ts/level": "/game/build/ts/level",
+    "logic": "/game/build/ts/logic",
+    "powerups": "/game/build/ts/powerups",
+    "../../ts/pp": "/game/build/ts/pp",
+    "progressWindow": "/game/build/ts/progressWindow",
+    "roundsGameLogic": "/game/build/ts/roundsGameLogic",
+    "../../ts/rpc": "/game/build/ts/rpc",
+    "scoreWindow": "/game/build/ts/scoreWindow",
+    "stickyWindow": "/game/build/ts/stickyWindow",
+    "../../ts/traceWindow": "/game/build/ts/traceWindow",
+    "tutorial": "/game/build/ts/tutorial",
+    "../../ts/util": "/game/build/ts/util",
+    "boogie": "/game/build/flowgame/ts/boogie",
+    "game_graph": "/game/build/flowgame/ts/game_graph",
+    "graph": "/game/build/flowgame/ts/graph",
+    "geometry": "/game/build/flowgame/ts/geometry",
+    "texticon": "/game/build/flowgame/ts/texticon",
+    "invgraph_game": "/game/build/flowgame/ts/invgraph_game",
+    "new_game": "/game/build/flowgame/ts/new_game",
+    "rpc": "/game/build/flowgame/ts/rpc"
+  },
+  shim: {
+    'phaser-ce': { exports: 'Phaser' },
+    'facebook-js-sdk': { exports: "FB" },
+    "bootstrap": { deps: ["jquery"] },
+    "jquery_ui": { deps: ["jquery"] },
+    "jquery_color": { deps: ["jquery"] },
+    "jsonrpcclient": { deps: ["jquery"] },
+    "rpc": { deps: ["jsonrpcclient"] }
+  },
+  waitSeconds: 0,
+});
+
+requirejs(
+  [
+    "bootstrap",
+    "new_game"
+  ],
+  () => {
+	console.log("Loaded...");
+  }
+);
